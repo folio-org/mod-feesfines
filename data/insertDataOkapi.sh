5fbd4c10c48f265817f8c68c361b10cb28f872cc
Lluvia Manilla <lmanillah@dgb.unam.mx>
2018-10-18 18:10:18 +0000

    Reorganize scripts to module

---
+++ data/insertDataOkapi.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+token=`cat dikuToken`
+
+curl -i -w '\n' -X POST -H 'Content-type: application/json' \
+    -H 'X-Okapi-Tenant: diku' -H "X-Okapi-Token: $token" -d @transfer.json http://localhost:9130/owners
+ 
+curl -i -w '\n' -X POST -H 'Content-type: application/json' \
+    -H 'X-Okapi-Tenant: diku' -H "X-Okapi-Token: $token" -d @comment.json http://localhost:9130/feefines
+
