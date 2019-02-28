babfd98e04598256def98fc4b03ffc4ea0e788fe
DGB2018 <lmanillah@dgb.unam.mx>
2019-02-28 21:46:13 +0000

    Fix code smell

--- src/main/java/org/folio/rest/utils/ValidationHelper.java
+++ src/main/java/org/folio/rest/utils/ValidationHelper.java
@@ -8,22 +8,25 @@ import org.folio.rest.jaxrs.model.Errors;
 import org.folio.rest.jaxrs.model.Parameter;
 import org.folio.rest.tools.RTFConsts;
 
-
 public class ValidationHelper {
 
-  public static Errors createValidationErrorMessage(String field, String value, String message){
-    Errors e = new Errors();
-    Error error = new Error();
-    Parameter p = new Parameter();
-    p.setKey(field);
-    p.setValue(value);
-    error.getParameters().add(p);
-    error.setMessage(message);
-    error.setCode("-1");
-    error.setType(RTFConsts.VALIDATION_FIELD_ERROR);
-    List<Error> l = new ArrayList<>();
-    l.add(error);
-    e.setErrors(l);
-    return e;
-  }
+    private ValidationHelper() {
+        throw new IllegalStateException("Utility class");
+    }
+
+    public static Errors createValidationErrorMessage(String field, String value, String message) {
+        Errors e = new Errors();
+        Error error = new Error();
+        Parameter p = new Parameter();
+        p.setKey(field);
+        p.setValue(value);
+        error.getParameters().add(p);
+        error.setMessage(message);
+        error.setCode("-1");
+        error.setType(RTFConsts.VALIDATION_FIELD_ERROR);
+        List<Error> l = new ArrayList<>();
+        l.add(error);
+        e.setErrors(l);
+        return e;
+    }
 }
