5fed9a0f2349da511300f5e144e9ebe926557404
DGB2018 <lmanillah@dgb.unam.mx>
2019-09-03 17:10:09 +0000

    Update data

--- data/curltoken_diku.sh
+++ data/curltoken_diku.sh
@@ -2,9 +2,8 @@
 token=`cat dikuToken`
 
 curl -D - -w '\n' -H "X-Okapi-Tenant: diku" -H "X-Okapi-Token: $token" http://localhost:9130/owners
+# /feefines Require ownerId
 curl -D - -w '\n' -H "X-Okapi-Tenant: diku" -H "X-Okapi-Token: $token" http://localhost:9130/feefines
-# /accounts Require add
 curl -D - -w '\n' -H "X-Okapi-Tenant: diku" -H "X-Okapi-Token: $token" http://localhost:9130/accounts
-#Depend to accounts
-curl -D - -w '\n' -H "X-Okapi-Tenant: diku" -H "X-Okapi-Token: $token" http://localhost:9130/feefinehistory
-curl -D - -w '\n' -H "X-Okapi-Tenant: diku" -H "X-Okapi-Token: $token" http://localhost:9130/chargeitem
+curl -D - -w '\n' -H "X-Okapi-Tenant: diku" -H "X-Okapi-Token: $token" http://localhost:9130/transfer-criterias
+curl -D - -w '\n' -H "X-Okapi-Tenant: diku" -H "X-Okapi-Token: $token" http://localhost:9130/overdue-fines-policies
