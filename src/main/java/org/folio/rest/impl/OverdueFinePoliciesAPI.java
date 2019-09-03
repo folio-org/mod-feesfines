f3c3e0c7df466ea027e4737711840a637dc54aa3
DGB2018 <lmanillah@dgb.unam.mx>
2019-09-03 18:26:10 +0000

    Remove unused logger

--- src/main/java/org/folio/rest/impl/OverdueFinePoliciesAPI.java
+++ src/main/java/org/folio/rest/impl/OverdueFinePoliciesAPI.java
@@ -3,8 +3,6 @@ package org.folio.rest.impl;
 import io.vertx.core.AsyncResult;
 import io.vertx.core.Context;
 import io.vertx.core.Handler;
-import io.vertx.core.logging.Logger;
-import io.vertx.core.logging.LoggerFactory;
 import java.util.Map;
 import javax.ws.rs.core.Response;
 import org.folio.rest.annotations.Validate;
