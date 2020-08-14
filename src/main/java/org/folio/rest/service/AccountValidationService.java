167bbaa3fd317232b201b9635b742493c5c93700
Roman_Barannyk <roman_barannyk@epam.com>
2020-08-14 13:47:22 +0300

    MODFEE-79 refactor to support existing code for other endpoint

--- src/main/java/org/folio/rest/service/AccountValidationService.java
+++ src/main/java/org/folio/rest/service/AccountValidationService.java
@@ -2,6 +2,7 @@ package org.folio.rest.service;
 
 import javax.ws.rs.core.Response;
 
+import org.folio.rest.jaxrs.model.Account;
 import org.folio.rest.jaxrs.model.AccountsCheckRequest;
 import org.folio.rest.jaxrs.model.AccountsCheckResponse;
 import org.folio.rest.jaxrs.resource.Accounts;
@@ -14,6 +15,7 @@ import io.vertx.core.Handler;
 public class AccountValidationService {
 
   private final AccountRepository accountRepository;
+  static final String PAYMENT_ACTION = "Payment";
 
   public AccountValidationService(AccountRepository accountRepository) {
     this.accountRepository = accountRepository;
@@ -24,28 +26,34 @@ public class AccountValidationService {
 
     accountRepository.getAccountById(accountId)
       .onSuccess(account -> {
-        double entityAmount = entity.getAmount();
-        if (entityAmount > account.getRemaining()) {
-          AccountsCheckResponse response = create422AccountCheckResponse(
-            accountId, entityAmount, "Payment amount exceeds the selected amount");
-          asyncResultHandler.handle(Future.succeededFuture(
-            Accounts.PostAccountsCheckPayByAccountIdResponse
-              .respond422WithApplicationJson(response)));
-        } else if (entityAmount < 0) {
-          AccountsCheckResponse response = create422AccountCheckResponse(
-            accountId, entityAmount, "Invalid amount entered");
-          asyncResultHandler.handle(Future.succeededFuture(
-            Accounts.PostAccountsCheckPayByAccountIdResponse
-              .respond422WithApplicationJson(response)));
-        } else {
+        ValidationResult validateResult = validate(account, entity, PAYMENT_ACTION);
+        if (validateResult.isAllowed) {
           AccountsCheckResponse response = create200AccountCheckResponse(
-            accountId, entityAmount, account.getRemaining());
+            accountId, entity.getAmount(), account.getRemaining());
           asyncResultHandler.handle(Future.succeededFuture(
             Accounts.PostAccountsCheckPayByAccountIdResponse
               .respond200WithApplicationJson(response)));
+        } else {
+          AccountsCheckResponse response = create422AccountCheckResponse(
+            accountId, entity.getAmount(), validateResult.getErrorMessage());
+          asyncResultHandler.handle(Future.succeededFuture(
+            Accounts.PostAccountsCheckPayByAccountIdResponse
+              .respond422WithApplicationJson(response)));
         }
       }).onFailure(e -> Future.succeededFuture(
         Accounts.PostAccountsCheckPayByAccountIdResponse.respond500WithTextPlain(e.getMessage())));
+  }
+
+  ValidationResult validate(Account account, AccountsCheckRequest request, String actionName) {
+    double entityAmount = request.getAmount();
+    if (entityAmount > account.getRemaining()) {
+      return new ValidationResult(false,
+        String.format("%s amount exceeds the selected amount", actionName));
+    } else if (entityAmount < 0) {
+      return new ValidationResult(false, "Invalid amount entered");
+    } else {
+      return new ValidationResult(true, account.getRemaining());
+    }
   }
 
   private AccountsCheckResponse create422AccountCheckResponse(String accountId,
@@ -73,5 +81,34 @@ public class AccountValidationService {
     response.setAccountId(accountId);
     response.setAmount(entityAmount);
     return response;
+  }
+
+  static class ValidationResult {
+
+    private boolean isAllowed;
+    private String errorMessage;
+    private Double remaining;
+
+    public ValidationResult(boolean isAllowed, String errorMessage) {
+      this.isAllowed = isAllowed;
+      this.errorMessage = errorMessage;
+    }
+
+    public ValidationResult(boolean isAllowed, Double remaining) {
+      this.isAllowed = isAllowed;
+      this.remaining = remaining;
+    }
+
+    public boolean isAllowed() {
+      return isAllowed;
+    }
+
+    public String getErrorMessage() {
+      return errorMessage;
+    }
+
+    public Double getRemaining() {
+      return remaining;
+    }
   }
 }
