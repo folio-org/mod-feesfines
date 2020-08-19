7acc92044c069140afcdea4690f5e953951df498
Roman_Barannyk <roman_barannyk@epam.com>
2020-08-19 14:10:23 +0300

    MODFEE-81 refactoring according to code review

--- src/test/java/org/folio/rest/impl/AccountsAPITest.java
+++ src/test/java/org/folio/rest/impl/AccountsAPITest.java
@@ -29,8 +29,6 @@ import org.folio.rest.domain.EventType;
 import org.folio.rest.jaxrs.model.Account;
 import org.folio.rest.jaxrs.model.Event;
 import org.folio.rest.jaxrs.model.EventMetadata;
-import org.folio.rest.jaxrs.model.PaymentStatus;
-import org.folio.rest.jaxrs.model.Status;
 import org.folio.test.support.ApiTests;
 import org.folio.test.support.matcher.TypeMappingMatcher;
 import org.folio.util.pubsub.PubSubClientUtils;
@@ -298,23 +296,6 @@ public class AccountsAPITest extends ApiTests {
 
     final JsonObject eventPayload = new JsonObject(event.getEventPayload());
     assertFalse(eventPayload.containsKey("loanId"));
-  }
-
-  Account createAccount() {
-    return new Account()
-      .withId(randomId())
-      .withOwnerId(randomId())
-      .withUserId(randomId())
-      .withItemId(randomId())
-      .withLoanId(randomId())
-      .withMaterialTypeId(randomId())
-      .withFeeFineId(randomId())
-      .withFeeFineType("book lost")
-      .withFeeFineOwner("owner")
-      .withAmount(9.00)
-      .withRemaining(4.55)
-      .withPaymentStatus(new PaymentStatus().withName("Outstanding"))
-      .withStatus(new Status().withName("Open"));
   }
 
   private JsonObject createAccountJsonObject(String accountID) {
