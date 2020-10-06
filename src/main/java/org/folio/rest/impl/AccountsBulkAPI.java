bf172b1e1fa530f44ee700e987f8692ed37ec5e6
Oleksandr Vidinieiev <Oleksandr_Vidinieiev@epam.com>
2020-10-06 14:51:50 +0300

    [MODFEE-110] Bulk transfer endpoint and tests

--- src/main/java/org/folio/rest/impl/AccountsBulkAPI.java
+++ src/main/java/org/folio/rest/impl/AccountsBulkAPI.java
@@ -18,6 +18,7 @@ import org.folio.rest.jaxrs.model.BulkCheckActionResponse;
 import org.folio.rest.jaxrs.model.DefaultBulkActionRequest;
 import org.folio.rest.jaxrs.resource.AccountsBulk;
 import org.folio.rest.service.action.BulkPayActionService;
+import org.folio.rest.service.action.BulkTransferActionService;
 import org.folio.rest.service.action.BulkWaiveActionService;
 import org.folio.rest.service.action.context.BulkActionContext;
 import org.folio.rest.service.action.validation.ActionValidationService;
@@ -77,6 +78,16 @@ public class AccountsBulkAPI implements AccountsBulk {
     new BulkWaiveActionService(okapiHeaders, vertxContext)
       .performAction(request)
       .onComplete(result -> handleActionResult(request, result, asyncResultHandler, Action.WAIVE));
+  }
+
+  @Override
+  public void postAccountsBulkTransfer(DefaultBulkActionRequest request,
+    Map<String, String> okapiHeaders, Handler<AsyncResult<Response>> asyncResultHandler,
+    Context vertxContext) {
+
+    new BulkTransferActionService(okapiHeaders, vertxContext)
+      .performAction(request)
+      .onComplete(result -> handleActionResult(request, result, asyncResultHandler, Action.TRANSFER));
   }
 
   private void checkBulkAction(BulkCheckActionRequest request,
