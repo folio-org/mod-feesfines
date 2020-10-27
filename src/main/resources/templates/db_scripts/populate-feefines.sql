fe521adb33ba84211f0a055ebf6ee7981e11abec
Bohdan Suprun <Bohdan_Suprun@epam.com>
2020-10-27 10:33:00 +0200

    MODFEE-114: Move Lost item fee (actual cost) into separate migration script.

--- src/main/resources/templates/db_scripts/populate-feefines.sql
+++ src/main/resources/templates/db_scripts/populate-feefines.sql
@@ -23,12 +23,3 @@ INSERT INTO feefines (id, jsonb) VALUES
  "feeFineType": "Lost item processing fee",
  "automatic": true
 }') ON CONFLICT DO NOTHING;
-
-INSERT INTO ${myuniversity}_${mymodule}.feefines (id, jsonb) VALUES
-( '73785370-d3bd-4d92-942d-ae2268e02ded',
-  '{
-   "id": "73785370-d3bd-4d92-942d-ae2268e02ded",
-   "feeFineType": "Lost item fee (actual cost)",
-   "automatic": true
-  }'
-) ON CONFLICT DO NOTHING;
