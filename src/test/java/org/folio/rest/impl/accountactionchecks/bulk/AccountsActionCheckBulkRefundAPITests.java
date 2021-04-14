be6879e7bdf3fd7f822c8df432301e07c076cde1
Alexander Kurash <Oleksandr_Kurash@epam.com>
2021-04-14 22:44:21 +0300

    MODFEE-182 Select feefineactions using criteria

--- src/test/java/org/folio/rest/impl/accountactionchecks/bulk/AccountsActionCheckBulkRefundAPITests.java
+++ src/test/java/org/folio/rest/impl/accountactionchecks/bulk/AccountsActionCheckBulkRefundAPITests.java
@@ -1,13 +1,19 @@
 package org.folio.rest.impl.accountactionchecks.bulk;
 
+import static io.restassured.http.ContentType.JSON;
 import static org.folio.rest.domain.Action.PAY;
 import static org.folio.rest.domain.Action.TRANSFER;
 import static org.folio.rest.utils.ResourceClients.buildAccountBulkCheckRefundClient;
 import static org.folio.rest.utils.ResourceClients.buildAccountCheckRefundClient;
 import static org.folio.rest.utils.ResourceClients.buildFeeFineActionsClient;
+import static org.hamcrest.CoreMatchers.is;
+
+import java.util.List;
+import java.util.UUID;
 
 import org.apache.http.HttpStatus;
 import org.folio.rest.impl.accountactionchecks.AccountsActionChecksAPITestsBase;
+import org.folio.rest.jaxrs.model.Account;
 import org.folio.rest.jaxrs.model.Feefineaction;
 import org.folio.rest.utils.ResourceClient;
 import org.junit.Before;
@@ -86,5 +92,49 @@ public class AccountsActionCheckBulkRefundAPITests extends AccountsActionChecksA
     removeAllFromTable(ACCOUNTS_TABLE);
     actionCheckShouldNotFailForNonExistentAccount(false, accountsCheckRefundClient);
     actionCheckShouldNotFailForNonExistentAccount(true, accountsBulkCheckRefundClient);
+  }
+
+  @Test
+  public void checkRefundAmountShouldBeCorrectWithSimilarAccountIds() {
+    String similarAccountId = firstAccount.getId().split("-")[0] + "-" +
+      String.join("-", List.of(UUID.randomUUID().toString().split("-")).subList(1, 5));
+    Account accountToPost = createAccount().withId(similarAccountId);
+    accountsClient.create(accountToPost)
+      .then()
+      .statusCode(HttpStatus.SC_CREATED)
+      .contentType(JSON);
+
+    final Feefineaction feeFineAction1 = new Feefineaction()
+      .withAccountId(firstAccount.getId())
+      .withUserId(firstAccount.getUserId())
+      .withAmountAction(5.0)
+      .withTypeAction(PAY.getPartialResult());
+
+    buildFeeFineActionsClient()
+      .post(feeFineAction1)
+      .then()
+      .statusCode(HttpStatus.SC_CREATED);
+
+    final Feefineaction feeFineAction2 = new Feefineaction()
+      .withAccountId(similarAccountId)
+      .withUserId(firstAccount.getUserId())
+      .withAmountAction(2.0)
+      .withTypeAction(PAY.getPartialResult());
+
+    buildFeeFineActionsClient()
+      .post(feeFineAction2)
+      .then()
+      .statusCode(HttpStatus.SC_CREATED);
+
+    String requestedAmount = "1.00";
+    final var request = createRequest(false, requestedAmount);
+    final var response = accountsCheckRefundClient.attemptCreate(request);
+    response
+      .then()
+      .statusCode(HttpStatus.SC_OK)
+      .body("allowed", is(true))
+      .body("accountId", is(firstAccount.getId()))
+      .body("amount", is(requestedAmount))
+      .body("remainingAmount", is("4.00"));
   }
 }
