a1db9c694b1a291a7c02f973c06a2b213ef4edc7
Alexander Kurash <Oleksandr_Kurash@epam.com>
2021-07-15 13:55:36 +0300

    MODFEE-179 Create report stats helper

--- src/main/java/org/folio/rest/service/report/FinancialTransactionsDetailReportService.java
+++ src/main/java/org/folio/rest/service/report/FinancialTransactionsDetailReportService.java
@@ -1,13 +1,13 @@
 package org.folio.rest.service.report;
 
 import static io.vertx.core.Future.succeededFuture;
-import static java.math.BigDecimal.ZERO;
 import static org.folio.rest.domain.Action.CANCEL;
 import static org.folio.rest.domain.Action.PAY;
 import static org.folio.rest.domain.Action.REFUND;
 import static org.folio.rest.domain.Action.TRANSFER;
 import static org.folio.rest.domain.Action.WAIVE;
 import static org.folio.rest.repository.FeeFineActionRepository.ORDER_BY_OWNER_SOURCE_DATE_ASC;
+import static org.folio.rest.service.report.utils.ReportStatsHelper.calculateTotals;
 import static org.folio.rest.utils.FeeFineActionHelper.getPatronInfoFromComment;
 import static org.folio.rest.utils.FeeFineActionHelper.getStaffInfoFromComment;
 import static org.folio.rest.utils.PatronHelper.buildFormattedName;
@@ -19,9 +19,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Objects;
 import java.util.function.Function;
-import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 import org.apache.logging.log4j.LogManager;
@@ -43,7 +41,6 @@ import org.folio.rest.jaxrs.model.LoanPolicy;
 import org.folio.rest.jaxrs.model.Location;
 import org.folio.rest.jaxrs.model.LostItemFeePolicy;
 import org.folio.rest.jaxrs.model.OverdueFinePolicy;
-import org.folio.rest.jaxrs.model.ReportTotalsEntry;
 import org.folio.rest.jaxrs.model.ServicePoint;
 import org.folio.rest.jaxrs.model.User;
 import org.folio.rest.jaxrs.model.UserGroup;
@@ -161,7 +158,6 @@ public class FinancialTransactionsDetailReportService extends
   }
 
   private FinancialTransactionsDetailReport buildReport(Context ctx) {
-
     List<FinancialTransactionsDetailReportEntry> entryList = ctx.actionsToAccounts.keySet().stream()
       .map(action -> buildReportEntry(ctx, action))
       .collect(Collectors.toList());
@@ -261,13 +257,17 @@ public class FinancialTransactionsDetailReportService extends
           }
 
           Loan loan = accountCtx.loan;
-          LoanPolicy loanPolicy = accountCtx.loanPolicy;
-          if (loan != null && loanPolicy != null) {
+          if (loan != null) {
             entry = entry
               .withLoanId(loan.getId())
               .withLoanDate(reformatLoanDate(loan.getLoanDate()))
-              .withLoanPolicyId(loan.getLoanPolicyId())
-              .withLoanPolicyName(loanPolicy.getName());
+              .withLoanPolicyId(loan.getLoanPolicyId());
+
+            LoanPolicy loanPolicy = accountCtx.loanPolicy;
+            if (loanPolicy != null) {
+              entry = entry
+                .withLoanPolicyName(loanPolicy.getName());
+            }
 
             OverdueFinePolicy overdueFinePolicy = accountCtx.overdueFinePolicy;
             if (overdueFinePolicy != null) {
@@ -344,59 +344,6 @@ public class FinancialTransactionsDetailReportService extends
       action -> getPaymentMethod(TRANSFER, action), "Transfer account totals");
 
     return stats;
-  }
-
-  private void calculateTotals(List<ReportTotalsEntry> totalsEntries, List<Feefineaction> actions,
-    Function<Feefineaction, String> categoryNameFunction, String totalsCategoryName) {
-
-    List<String> categories = actions.stream()
-      .map(categoryNameFunction)
-      .filter(Objects::nonNull)
-      .filter(category -> !category.isEmpty())
-      .distinct()
-      .collect(Collectors.toList());
-
-    // Calculate categories
-    categories.forEach(category -> totalsEntries.add(new ReportTotalsEntry()
-      .withName(category)
-      .withTotalAmount(actions.stream()
-        .filter(filterByCategory(category, categoryNameFunction))
-        .map(Feefineaction::getAmountAction)
-        .filter(Objects::nonNull)
-        .map(MonetaryValue::new)
-        .reduce(MonetaryValue::add)
-        .orElse(new MonetaryValue(ZERO))
-        .toString())
-      .withTotalCount(String.valueOf(actions.stream()
-        .filter(filterByCategory(category, categoryNameFunction))
-        .count()))));
-
-    // Calculate total
-    totalsEntries.add(new ReportTotalsEntry()
-      .withName(totalsCategoryName)
-      .withTotalAmount(actions.stream()
-        .filter(filterByCategories(categories, categoryNameFunction))
-        .map(Feefineaction::getAmountAction)
-        .filter(Objects::nonNull)
-        .map(MonetaryValue::new)
-        .reduce(MonetaryValue::add)
-        .orElse(new MonetaryValue(ZERO))
-        .toString())
-      .withTotalCount(String.valueOf(actions.stream()
-        .filter(filterByCategories(categories, categoryNameFunction))
-        .count())));
-  }
-
-  private Predicate<Feefineaction> filterByCategory(String category,
-    Function<Feefineaction, String> categoryNameFunction) {
-
-    return action -> category.equals(categoryNameFunction.apply(action));
-  }
-
-  private Predicate<Feefineaction> filterByCategories(List<String> categories,
-    Function<Feefineaction, String> categoryNameFunction) {
-
-    return action -> categories.contains(categoryNameFunction.apply(action));
   }
 
   private String formatMonetaryValue(Double value) {
