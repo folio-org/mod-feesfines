3f8cec74f54c3891268ed6bf3787cdb55f40f8ec
Alexander Kurash <Oleksandr_Kurash@epam.com>
2021-08-04 20:39:07 +0300

    Resolve conflict with MODFEE-203

--- src/main/java/org/folio/rest/repository/FeeFineActionRepository.java
+++ src/main/java/org/folio/rest/repository/FeeFineActionRepository.java
@@ -12,6 +12,7 @@ import java.util.Collection;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
 import org.folio.rest.domain.Action;
