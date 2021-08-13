14117020001118ecdf6847a70e234f380e82bcf1
Oleksandr Vidinieiev <Oleksandr_Vidinieiev@epam.com>
2021-08-13 11:10:57 +0300

    [MODFEE-200] Use Lombok in FeeFineNoticeContext

--- src/main/java/org/folio/rest/domain/FeeFineNoticeContext.java
+++ src/main/java/org/folio/rest/domain/FeeFineNoticeContext.java
@@ -12,6 +12,16 @@ import org.folio.rest.jaxrs.model.Location;
 import org.folio.rest.jaxrs.model.Owner;
 import org.folio.rest.jaxrs.model.User;
 
+import io.vertx.core.json.JsonObject;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.With;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@With
 public class FeeFineNoticeContext {
 
   private Owner owner;
@@ -24,25 +34,7 @@ public class FeeFineNoticeContext {
   private Instance instance;
   private HoldingsRecord holdingsRecord;
   private Location effectiveLocation;
-
-  public FeeFineNoticeContext() {
-  }
-
-  public FeeFineNoticeContext(Owner owner, Feefine feefine, Account account,
-    Feefineaction charge, Feefineaction action, User user, Item item, Instance instance,
-    HoldingsRecord holdingsRecord, Location effectiveLocation) {
-
-    this.owner = owner;
-    this.feefine = feefine;
-    this.account = account;
-    this.charge = charge;
-    this.action = action;
-    this.user = user;
-    this.item = item;
-    this.instance = instance;
-    this.holdingsRecord = holdingsRecord;
-    this.effectiveLocation = effectiveLocation;
-  }
+  private JsonObject logEventPayload;
 
   public String getTemplateId() {
     if (action != null) {
@@ -76,93 +68,4 @@ public class FeeFineNoticeContext {
     return getPrimaryAction().getAccountId();
   }
 
-  public FeeFineNoticeContext withOwner(Owner owner) {
-    return new FeeFineNoticeContext(owner, feefine, account, charge, action, user, item,
-      instance, holdingsRecord, effectiveLocation);
-  }
-
-  public FeeFineNoticeContext withFeefine(Feefine feefine) {
-    return new FeeFineNoticeContext(owner, feefine, account, charge, action, user, item,
-      instance, holdingsRecord, effectiveLocation);
-  }
-
-  public FeeFineNoticeContext withAccount(Account account) {
-    return new FeeFineNoticeContext(owner, feefine, account, charge, action, user, item,
-      instance, holdingsRecord, effectiveLocation);
-  }
-
-  public FeeFineNoticeContext withCharge(Feefineaction charge) {
-    return new FeeFineNoticeContext(owner, feefine, account, charge, action, user, item,
-      instance, holdingsRecord, effectiveLocation);
-  }
-
-  public FeeFineNoticeContext withAction(Feefineaction action) {
-    return new FeeFineNoticeContext(owner, feefine, account, charge, action, user, item,
-      instance, holdingsRecord, effectiveLocation);
-  }
-
-  public FeeFineNoticeContext withUser(User user) {
-    return new FeeFineNoticeContext(owner, feefine, account, charge, action, user, item,
-      instance, holdingsRecord, effectiveLocation);
-  }
-
-  public FeeFineNoticeContext withItem(Item item) {
-    return new FeeFineNoticeContext(owner, feefine, account, charge, action, user, item,
-      instance, holdingsRecord, effectiveLocation);
-  }
-
-  public FeeFineNoticeContext withInstance(Instance instance) {
-    return new FeeFineNoticeContext(owner, feefine, account, charge, action, user, item,
-      instance, holdingsRecord, effectiveLocation);
-  }
-
-  public FeeFineNoticeContext withHoldingsRecord(HoldingsRecord holdingsRecord) {
-    return new FeeFineNoticeContext(owner, feefine, account, charge, action, user, item,
-      instance, holdingsRecord, effectiveLocation);
-  }
-
-  public FeeFineNoticeContext withEffectiveLocation(Location effectiveLocation) {
-    return new FeeFineNoticeContext(owner, feefine, account, charge, action, user, item,
-      instance, holdingsRecord, effectiveLocation);
-  }
-
-  public Owner getOwner() {
-    return owner;
-  }
-
-  public Feefine getFeefine() {
-    return feefine;
-  }
-
-  public Account getAccount() {
-    return account;
-  }
-
-  public Feefineaction getCharge() {
-    return charge;
-  }
-
-  public Feefineaction getAction() {
-    return action;
-  }
-
-  public User getUser() {
-    return user;
-  }
-
-  public Item getItem() {
-    return item;
-  }
-
-  public Instance getInstance() {
-    return instance;
-  }
-
-  public HoldingsRecord getHoldingsRecord() {
-    return holdingsRecord;
-  }
-
-  public Location getEffectiveLocation() {
-    return effectiveLocation;
-  }
 }
