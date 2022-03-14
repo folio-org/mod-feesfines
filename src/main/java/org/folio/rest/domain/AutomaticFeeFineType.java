1736520fe13701d35c4b75a2857a459048860d48
Sherzod Nurjonov <jd.sherzod@gmail.com>
2022-03-14 18:50:54 +0500

    MODFEE-243 implemented (#219)

---
+++ src/main/java/org/folio/rest/domain/AutomaticFeeFineType.java
@@ -0,0 +1,34 @@
+package org.folio.rest.domain;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public enum AutomaticFeeFineType {
+  // IDs come from /resources/templates/db_scripts/populate-feefines.sql
+
+  OVERDUE_FINE("9523cb96-e752-40c2-89da-60f3961a488d"),
+  REPLACEMENT_PROCESSING_FEE("d20df2fb-45fd-4184-b238-0d25747ffdd9"),
+  LOST_ITEM_FEE("cf238f9f-7018-47b7-b815-bb2db798e19f"),
+  LOST_ITEM_PROCESSING_FEE("c7dede15-aa48-45ed-860b-f996540180e0");
+
+  private final String id;
+  private static final Map<String, AutomaticFeeFineType> idIndex = new HashMap<>(AutomaticFeeFineType.values().length);
+
+  static {
+    for (AutomaticFeeFineType automaticFeeFineType : AutomaticFeeFineType.values()) {
+      idIndex.put(automaticFeeFineType.id, automaticFeeFineType);
+    }
+  }
+
+  AutomaticFeeFineType(String id) {
+    this.id = id;
+  }
+
+  public String getId() {
+    return id;
+  }
+
+  public static AutomaticFeeFineType getById(String id) {
+    return idIndex.get(id);
+  }
+}
--- src/test/java/org/folio/rest/impl/FeeFinesAPITest.java
+++ src/test/java/org/folio/rest/impl/FeeFinesAPITest.java
@@ -1,14 +1,19 @@
 package org.folio.rest.impl;
 
 import org.apache.http.HttpStatus;
+import org.folio.rest.domain.AutomaticFeeFineType;
 import org.folio.test.support.ApiTests;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 import io.vertx.core.json.JsonObject;
+import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
 
+@RunWith(JUnitParamsRunner.class)
 public class FeeFinesAPITest extends ApiTests {
   private static final String REST_PATH = "/feefines";
   private static final String FEEFINES_TABLE = "feefines";
@@ -92,6 +97,15 @@ public class FeeFinesAPITest extends ApiTests {
     post(entity2)
       .then()
       .statusCode(HttpStatus.SC_CREATED)
+      .contentType(ContentType.JSON);
+  }
+
+  @Test
+  @Parameters(source = AutomaticFeeFineType.class)
+  public void cannotDeleteAutomaticFeeFineType(AutomaticFeeFineType automaticFeeFineType) {
+    client.delete(REST_PATH + "/" + automaticFeeFineType.getId())
+      .then()
+      .statusCode(HttpStatus.SC_UNPROCESSABLE_ENTITY)
       .contentType(ContentType.JSON);
   }
 
