1a178b41781692349d571e174283eac55e261ea6
Yusuf Murodov <yusuf_murodov@epam.com>
2022-03-15 18:50:09 +0500

    reformatted code

--- src/test/java/org/folio/rest/domain/AccountPaymentStatusTest.java
+++ src/test/java/org/folio/rest/domain/AccountPaymentStatusTest.java
@@ -12,7 +12,6 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
-
 class AccountPaymentStatusTest {
 
   @ParameterizedTest
