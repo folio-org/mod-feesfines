d1ed0fbbcda3350147f3a82b8c5bb7a23cf65a2d
Niels Erik Nielsen <nielserik@indexdata.com>
2023-06-21 19:26:05 +0200

    Remove a previous migration test (MODFEE-358)
      - aged to obsolete

--- src/test/java/org/folio/migration/FeeFineTypesDefaultReferenceRecordsTest.java
+++ src/test/java/org/folio/migration/FeeFineTypesDefaultReferenceRecordsTest.java
@@ -1,6 +1,5 @@
 package org.folio.migration;
 
-import static org.folio.test.support.matcher.FeeFineMatchers.hasAllAutomaticFeeFineTypesFor15_10;
 import static org.folio.test.support.matcher.FeeFineMatchers.hasAllAutomaticFeeFineTypesFor18_3;
 import static org.folio.util.PomUtils.getModuleVersion;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -17,24 +16,8 @@ import org.junit.jupiter.api.Test;
 import io.vertx.core.Vertx;
 
 public class FeeFineTypesDefaultReferenceRecordsTest extends ApiTests {
-  private static final String MIGRATION_SCRIPT_15_9_TO_15_10 = loadMigrationScript15_9to15_10();
   private static final String MIGRATION_SCRIPT_18_2_TO_18_3 = loadMigrationScript18_2to18_3();
 
-  @Test
-  void lostItemFeeForActualCostIsAddedWhenMigratingFrom15_9To15_10() {
-    // module was enabled in @BeforeAll with moduleTo=current_version
-    // we must downgrade to 15.9.0 first if we want to rerun the migration script (see RMB-937)
-    createTenant(getModuleVersion(), "15.9.0");
-
-    // use SQL to delete, API refuses deleting automatic type
-    var deleted = get(PostgresClient.getInstance(vertx, TENANT_NAME)
-        .delete(FEEFINES_TABLE, AutomaticFeeFineType.LOST_FEE_FOR_ACTUAL_COST.getId()));
-    assertThat(deleted.rowCount(), is(1));
-    get(PostgresClient.getInstance(vertx, TENANT_NAME)
-      .delete(FEEFINES_TABLE, AutomaticFeeFineType.REMINDER_FEE.getId()));
-    createTenant("15.9.0", "15.10.0");
-    feeFinesClient.getAll().then().body(hasAllAutomaticFeeFineTypesFor15_10());
-  }
 
   @Test
   void reminderFeeIsAddedWhenMigratingFrom18_2To18_3() {
@@ -62,30 +45,10 @@ public class FeeFineTypesDefaultReferenceRecordsTest extends ApiTests {
   }
 
   @Test
-  void subsequentRunOfMigrationsDoesNotCauseIssues() {
-    executeMigration_15_9_to_15_10();
-
-    feeFinesClient.getAll().then()
-      .body(hasAllAutomaticFeeFineTypesFor15_10());
-
+  void subsequentRunOfMigrationDoesNotCauseIssues() {
     executeMigration_18_2_to_18_3();
-
     feeFinesClient.getAll().then()
       .body(hasAllAutomaticFeeFineTypesFor18_3());
-  }
-
-  private static String loadMigrationScript15_9to15_10() {
-    try (final var resourceAsStream = FeeFineTypesDefaultReferenceRecordsTest.class
-      .getResourceAsStream("/templates/db_scripts/" +
-        "add-lost-fee-for-actual-cost.sql")) {
-
-      return new String(resourceAsStream.readAllBytes(), StandardCharsets.UTF_8)
-        .replaceAll("\\$\\{myuniversity}", TENANT_NAME)
-        .replaceAll("\\$\\{mymodule}", MODULE_NAME.replace("-", "_"));
-
-    } catch (Exception ex) {
-      throw new RuntimeException(ex);
-    }
   }
 
   private static String loadMigrationScript18_2to18_3() {
@@ -102,23 +65,8 @@ public class FeeFineTypesDefaultReferenceRecordsTest extends ApiTests {
     }
   }
 
-  private void executeMigration_15_9_to_15_10() {
-    final var future = new CompletableFuture<Void>();
-
-    PostgresClient.getInstance(Vertx.vertx(), TENANT_NAME)
-      .execute(MIGRATION_SCRIPT_15_9_TO_15_10, result -> {
-        if (result.succeeded()) {
-          future.complete(null);
-        } else {
-          future.completeExceptionally(result.cause());
-        }
-      });
-
-    get(future);
-  }
-
   private void executeMigration_18_2_to_18_3() {
-    final var future = new CompletableFuture<Void>();
+    final var future = new CompletableFuture<>();
 
     PostgresClient.getInstance(Vertx.vertx(), TENANT_NAME)
       .execute(MIGRATION_SCRIPT_18_2_TO_18_3, result -> {
--- src/test/java/org/folio/test/support/matcher/FeeFineMatchers.java
+++ src/test/java/org/folio/test/support/matcher/FeeFineMatchers.java
@@ -13,33 +13,6 @@ public final class FeeFineMatchers {
   private FeeFineMatchers() {}
 
   @SuppressWarnings("all")
-  public static Matcher<?> hasAllAutomaticFeeFineTypesFor15_10() {
-    return hasJsonPath("feefines", allOf(iterableWithSize(6),
-      hasItems(
-        allOf(
-          hasJsonPath("id", is("9523cb96-e752-40c2-89da-60f3961a488d")),
-          hasJsonPath("automatic", is(true)),
-          hasJsonPath("feeFineType", is("Overdue fine"))),
-        allOf(
-          hasJsonPath("id", is("cf238f9f-7018-47b7-b815-bb2db798e19f")),
-          hasJsonPath("automatic", is(true)),
-          hasJsonPath("feeFineType", is("Lost item fee"))),
-        allOf(
-          hasJsonPath("id", is("73785370-d3bd-4d92-942d-ae2268e02ded")),
-          hasJsonPath("automatic", is(true)),
-          hasJsonPath("feeFineType", is("Lost item fee (actual cost)"))),
-        allOf(
-          hasJsonPath("id", is("c7dede15-aa48-45ed-860b-f996540180e0")),
-          hasJsonPath("automatic", is(true)),
-          hasJsonPath("feeFineType", is("Lost item processing fee"))),
-        allOf(
-          hasJsonPath("id", is("d20df2fb-45fd-4184-b238-0d25747ffdd9")),
-          hasJsonPath("automatic", is(true)),
-          hasJsonPath("feeFineType", is("Replacement processing fee")))
-      )));
-  }
-
-  @SuppressWarnings("all")
   public static Matcher<?> hasAllAutomaticFeeFineTypesFor18_3() {
     return hasJsonPath("feefines", allOf(iterableWithSize(6),
       hasItems(
