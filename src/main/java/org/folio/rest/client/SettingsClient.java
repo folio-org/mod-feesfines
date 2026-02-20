d379106f67bed4196c9c6995aa05ee6aa187dced
Roman Barannyk <53909129+roman-barannyk@users.noreply.github.com>
2026-02-20 14:18:27 +0200

    MODFEE-441 Use GET /locale to get tenant language & locale setting se… (#355)
    * MODFEE-439 Use GET /locale to get tenant language & locale setting selections
    
    * MODFEE-441 fix code smells
    
    * MODFEE-441 refactoring, logging improvement
    
    * MODFEE-441 add methods name to logging
    
    * MODFEE-441 add module permission

--- src/main/java/org/folio/rest/client/SettingsClient.java
+++ src/main/java/org/folio/rest/client/SettingsClient.java
@@ -3,7 +3,6 @@ package org.folio.rest.client;
 import static io.vertx.core.Future.failedFuture;
 import static io.vertx.core.Future.succeededFuture;
 import static io.vertx.core.http.HttpMethod.GET;
-import static java.lang.String.format;
 
 import java.util.Map;
 
@@ -11,9 +10,6 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.folio.rest.domain.LocaleSettings;
 import org.folio.rest.exception.http.HttpException;
-import org.folio.rest.jaxrs.model.Setting;
-import org.folio.rest.jaxrs.model.Settings;
-import org.folio.util.StringUtil;
 
 import io.vertx.core.Future;
 import io.vertx.core.Vertx;
@@ -21,17 +17,14 @@ import io.vertx.core.json.JsonObject;
 
 public class SettingsClient extends OkapiClient {
   private static final Logger log = LogManager.getLogger(SettingsClient.class);
+  public static final String LOCALE_URL = "/locale";
 
   public SettingsClient(Vertx vertx, Map<String, String> okapiHeaders) {
     super(vertx, okapiHeaders);
   }
 
   public Future<LocaleSettings> getLocaleSettings() {
-    String query = cqlAnd(cqlExactMatch("scope", "stripes-core.prefs.manage"),
-      cqlExactMatch("key", "tenantLocaleSettings"));
-
-    String url = format("/settings/entries?query=%s", StringUtil.urlEncode(query));
-    return okapiGetAbs(url).send()
+    return okapiGetAbs(LOCALE_URL).send()
       .compose(response -> {
         int responseStatus = response.statusCode();
         if (responseStatus != 200) {
@@ -39,42 +32,30 @@ public class SettingsClient extends OkapiClient {
             "Failed to find locale settings. Response: %d %s", responseStatus,
             response.bodyAsString());
           log.error(errorMessage);
-          return failedFuture(new HttpException(GET, url, response));
+          return failedFuture(new HttpException(GET, LOCALE_URL, response));
         } else {
           try {
-            Settings settings = objectMapper.readValue(response.bodyAsString(), Settings.class);
-
-            JsonObject localeSettingsJsonObject = settings.getItems()
-              .stream()
-              .findFirst()
-              .map(Setting::getValue)
-              .filter(Map.class::isInstance)
-              .map(Map.class::cast)
-              .map(JsonObject::new)
-              .orElse(null);
-
-            if (localeSettingsJsonObject == null) {
+            String localSettings = response.bodyAsString();
+            if (localSettings == null) {
+              log.warn("getLocaleSettings:: locale settings response body is null");
               return failedFuture("Failed to find locale settings");
-            } else {
-              return succeededFuture(new LocaleSettings(
-                localeSettingsJsonObject.getString("locale"),
-                localeSettingsJsonObject.getString("timezone"),
-                localeSettingsJsonObject.getString("currency")
-              ));
             }
+
+            JsonObject localeSettingsJsonObject = new JsonObject(localSettings);
+
+            String locale = localeSettingsJsonObject.getString("locale");
+            String timezone = localeSettingsJsonObject.getString("timezone");
+            String currency = localeSettingsJsonObject.getString("currency");
+            String numberingSystem = localeSettingsJsonObject.getString("numberingSystem");
+            log.info("getLocaleSettings:: locale settings found: locale: {}, timezone: {}, currency: {}, numberingSystem: {}",
+              locale, timezone, currency, numberingSystem);
+
+            return succeededFuture(new LocaleSettings(locale, timezone, currency, numberingSystem));
           } catch (Exception e) {
-            log.error("Failed to parse response: {}", response.bodyAsString(), e);
+            log.error("getLocaleSettings:: failed to parse response: {}", response.bodyAsString(), e);
             return failedFuture(e);
           }
         }
       });
-  }
-
-  private String cqlExactMatch(String index, String value) {
-    return format("%s==\"%s\"", index, value);
-  }
-
-  private String cqlAnd(String left, String right) {
-    return format("%s and %s", left, right);
   }
 }
--- src/test/java/org/folio/test/support/EntityBuilder.java
+++ src/test/java/org/folio/test/support/EntityBuilder.java
@@ -9,7 +9,6 @@ import java.util.Collections;
 import java.util.Date;
 import java.util.HashSet;
 import java.util.List;
-import java.util.UUID;
 
 import org.folio.rest.domain.MonetaryValue;
 import org.folio.rest.jaxrs.model.Account;
@@ -36,15 +35,10 @@ import org.folio.rest.jaxrs.model.PaymentStatus;
 import org.folio.rest.jaxrs.model.Personal;
 import org.folio.rest.jaxrs.model.Publication;
 import org.folio.rest.jaxrs.model.ReportTotalsEntry;
-import org.folio.rest.jaxrs.model.ResultInfo;
 import org.folio.rest.jaxrs.model.ServicePoint;
-import org.folio.rest.jaxrs.model.Setting;
-import org.folio.rest.jaxrs.model.Settings;
 import org.folio.rest.jaxrs.model.Status;
 import org.folio.rest.jaxrs.model.User;
 import org.folio.rest.jaxrs.model.UserGroup;
-
-import io.vertx.core.json.JsonObject;
 
 public class EntityBuilder {
   private static final String KEY_NAME = "name";
@@ -257,16 +251,6 @@ public class EntityBuilder {
     return new Institution()
       .withAdditionalProperty(KEY_ID, randomId())
       .withAdditionalProperty(KEY_NAME, "Institution");
-  }
-
-  public static Settings buildLocaleSettings(JsonObject value) {
-    return new Settings()
-      .withItems(List.of(new Setting()
-        .withId(UUID.randomUUID())
-        .withScope("stripes-core.prefs.manage")
-        .withKey("tenantLocaleSettings")
-        .withValue(value)))
-      .withResultInfo(new ResultInfo().withTotalRecords(1));
   }
 
   public static Loan buildLoan(String loanDate, Date dueDate, String returnDate, String itemId,
